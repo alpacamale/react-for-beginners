3e9b982292333938e8cb0700d78d575d1772b363
AlpacaMale <alpaca9100@gmail.com>
2025-08-31 13:55:02 +0900

    useParams

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,21 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState("");
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return <div>{loading ? <h1>Loading ...</h1> : <h1>hi</h1>}</div>;
 }
 
 export default Detail;
